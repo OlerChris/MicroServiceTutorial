f4ec328c91e539c6f989c12209c6be91fd21f2ad
Chris <olerchris@gmail.com>
2017-11-14 14:50:22 -0500

    converter for publicAccess

--- src/main/java/com/Revature/MessagingService/repositories/converters/GroupPublicAccess.java
+++ src/main/java/com/Revature/MessagingService/repositories/converters/GroupPublicAccess.java
@@ -1,4 +1,17 @@
 package com.Revature.MessagingService.repositories.converters;
 
-public class GroupPublicAccess {
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class GroupPublicAccess implements AttributeConverter<Boolean,Character> {
+    @Override
+    public Character convertToDatabaseColumn(Boolean aBoolean) {
+        return aBoolean ? 'T' : 'F';
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(Character character) {
+        return character == 'T';
+    }
 }
