af1c07df289b2a85c54266ccf8bf3aa92dd2930a
Chris <olerchris@gmail.com>
2017-11-16 15:56:52 -0500

    ADDED seq

--- src/main/java/com/Revature/AccountService/controllers/UserController.java
+++ src/main/java/com/Revature/AccountService/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.Revature.AccountService.controllers;
 
 import com.Revature.AccountService.beans.User;
+import com.Revature.AccountService.beans.enums.SecurityLevel;
 import com.Revature.AccountService.services.UserServive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,13 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/accountService/user")
 public class UserController {
 
+    public UserController(){}
+
     @Autowired
     private UserServive userService;
 
     @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity register(@RequestBody User in){
-        User u;
-        u = in;
+    public ResponseEntity register(){
+        User u = new User();
+        u.setFirstName("John");
+        u.setLastName("Johnson");
+        u.setPword("JJ");
+        u.setSecurityLevel(SecurityLevel.Basic);
         u = userService.register(u);
         return new ResponseEntity<User>(u, HttpStatus.ACCEPTED);
     }
