7983aceeed04956f0f746beb546ed2e390f85805
Chris <olerchris@gmail.com>
2017-11-16 14:55:13 -0500

    registraton

--- src/main/java/com/Revature/AccountService/controllers/UserController.java
+++ src/main/java/com/Revature/AccountService/controllers/UserController.java
@@ -1,9 +1,28 @@
 package com.Revature.AccountService.controllers;
 
+import com.Revature.AccountService.beans.User;
+import com.Revature.AccountService.services.UserServive;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/accountService/user")
 public class UserController {
+
+    @Autowired
+    private UserServive userService;
+
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity register(@RequestBody User in){
+        User u;
+        u = in;
+        u = userService.register(u);
+        return new ResponseEntity<User>(u, HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/Revature/AccountService/services/UserServive.java
+++ src/main/java/com/Revature/AccountService/services/UserServive.java
@@ -1,7 +1,17 @@
 package com.Revature.AccountService.services;
 
+import com.Revature.AccountService.beans.User;
+import com.Revature.AccountService.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServive {
+
+    @Autowired
+    private UserRepo repo;
+
+    public User register(User persisted){
+        return repo.save(persisted);
+    }
 }
