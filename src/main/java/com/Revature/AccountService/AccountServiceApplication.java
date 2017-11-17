e772775407c1abd642ee49d157107521b33e618d
Chris <olerchris@gmail.com>
2017-11-16 23:09:20 -0500

    Restructure

--- src/main/java/com/Revature/AccountService/AccountServiceApplication.java
+++ src/main/java/com/Revature/AccountService/AccountServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.config.name", "AccountService");
+		System.setProperty("spring.config.name", "properties/AccountService");
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
 }
