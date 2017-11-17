f001f2ac137bf6e1b6ffbc48401efecd6197ab24
Chris <olerchris@gmail.com>
2017-11-16 23:02:35 -0500

    Group Bean

--- src/main/java/com/Revature/MessagingService/beans/Group.java
+++ src/main/java/com/Revature/MessagingService/beans/Group.java
@@ -17,6 +17,38 @@ public class Group {
     @Column(name="Group_Owner")
     private long owner;
 
-    @Column(name="Public_Access") //TODO converter
+    @Column(name="Public_Access")
     private boolean publicAccess;
+
+    public Group(){}
+
+    public Group(long groupId, long owner, boolean publicAccess) {
+        this.groupId = groupId;
+        this.owner = owner;
+        this.publicAccess = publicAccess;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public long getOwner() {
+        return owner;
+    }
+
+    public void setOwner(long owner) {
+        this.owner = owner;
+    }
+
+    public boolean isPublicAccess() {
+        return publicAccess;
+    }
+
+    public void setPublicAccess(boolean publicAccess) {
+        this.publicAccess = publicAccess;
+    }
 }
