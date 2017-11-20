3e1bc9dc130ff61f9b96897d34d0f77429a95998
Chris <olerchris@gmail.com>
2017-11-20 10:35:23 -0500

    zuul debugging

--- src/main/java/com/Revature/MessagingService/controllers/MessageController.java
+++ src/main/java/com/Revature/MessagingService/controllers/MessageController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/messagingService/message")
+@RequestMapping("/message")
 public class MessageController {
 
     @Autowired
