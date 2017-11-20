392fc22f4ff1553336ca7cb14efcefd5ca2276ff
Chris <olerchris@gmail.com>
2017-11-20 12:46:45 -0500

    ZUUL

--- src/main/java/com/Revature/AccountService/AccountServiceApplication.java
+++ src/main/java/com/Revature/AccountService/AccountServiceApplication.java
@@ -3,10 +3,12 @@ package com.Revature.AccountService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
